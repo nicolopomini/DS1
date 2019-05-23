909dd330ddf90e8960d723407e878bdf9e3982c3
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2019-05-23 19:59:42 +0200

    Small changes in the manager, plus added the handler for the inputs from stdin

--- src/main/java/it/unitn/disi/ds1/martini_pomini/Manager.java
+++ src/main/java/it/unitn/disi/ds1/martini_pomini/Manager.java
@@ -13,6 +13,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 /**
  *
@@ -20,16 +21,21 @@ import java.util.Hashtable;
  */
 public class Manager {
     public static final String DEFAULT_FILE = "src/main/resources/topology.txt";
+    public static final String COMMAND_HELP = "h";
+    public static final String COMMAND_INJECT = "i";
+    public static final String COMMAND_REQUEST = "r";
+    public static final String COMMAND_EXIT = "q";
     
     private Hashtable<Integer, ArrayList<ActorRef>> edges;
-    private ArrayList<ActorRef> nodes;
+    private Hashtable<Integer, ActorRef> nodes;
     private final ActorSystem system;
     
     public Manager(String filepath) {
         this.edges = new Hashtable<>();
-        this.nodes = new ArrayList<>();
+        this.nodes = new Hashtable<>();
         this.system = ActorSystem.create("ds1project");
         try {
+            System.out.println("Building the system with the topology specified in " + filepath);
             BufferedReader inputFile = new BufferedReader(new FileReader(filepath));
             int links = Integer.parseInt(inputFile.readLine());
             for (int i = 0; i < links; i++) {
@@ -38,16 +44,17 @@ public class Manager {
                 int node2id = Integer.parseInt(link[1]);
                 // add the nodes if is the first time they are met
                 if (!this.edges.containsKey(node1id)) {
-                    this.nodes.add(this.system.actorOf(Node.props(node1id)));
+                    this.nodes.put(node1id, this.system.actorOf(Node.props(node1id)));
                     this.edges.put(node1id, new ArrayList<>());
                 }
                 if (!this.edges.containsKey(node2id)) {
-                    this.nodes.add(this.system.actorOf(Node.props(node2id)));
+                    this.nodes.put(node2id, this.system.actorOf(Node.props(node2id)));
                     this.edges.put(node2id, new ArrayList<>());
                 }
                 // adding the connections
                 this.edges.get(node1id).add(this.nodes.get(node2id));
                 this.edges.get(node2id).add(this.nodes.get(node1id));
+                // send the neighbors to each node
             }
         } catch (FileNotFoundException ex) {
             System.err.println("Error in opening the topology file. " + ex.getMessage());
@@ -59,5 +66,30 @@ public class Manager {
     
     public Manager() {
         this(Manager.DEFAULT_FILE);
+    }
+    
+    public void printCommands() {
+        System.out.println("List of commands:");
+        System.out.println("\t" + Manager.COMMAND_HELP + " -> print the list of commands");
+        System.out.println("\t" + Manager.COMMAND_INJECT + " -> inject the token into a node");
+        System.out.println("\t" + Manager.COMMAND_REQUEST + " -> request critical section access for a node");
+        System.out.println("\t" + Manager.COMMAND_EXIT + " -> quit");
+        System.out.println();
+    }
+    
+    public void handleCommands() {
+        boolean goOn = true;
+        Scanner in = new Scanner(System.in);
+        this.printCommands();
+        while(goOn) {
+            String command = in.next();
+            switch(command) {
+                case Manager.COMMAND_EXIT:
+                    goOn = false;
+                    break;
+                default: 
+                    System.out.println("This command doesn't exist");
+            }
+        }
     }
 }
